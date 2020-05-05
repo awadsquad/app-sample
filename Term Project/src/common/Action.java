4cdd201c3e398871ce4c17442aed090dda9bc334
Sara <Sara@BB8>
2020-05-05 04:00:58 -0400

    things that go bump in the night....bad code

--- Term Project/src/common/Action.java
+++ Term Project/src/common/Action.java
@@ -8,19 +8,13 @@ public class Action {
 	public static final String GET_FLIGHTS = "getflight";
 	public static final String BOOK_FLIGHT = "bookflight";*/
 
-
-	
-	
-	
 //method to book a flight
-public static void bookFlight(Customer cust, Flight flight) {
+public static void bookFlight(Customer cust) {
 	String selectedflight = "null";
 	//select a flight	
+	//ArrayList<Flights> flights = cust.getFlights();
+		
 	
-	ArrayList<Flights> flights = cust.getFlights();
-			
-	
-	//call methods
 	for (int i = 0; i < flights.length; i++) {
 		
 		if (flights[i].)
@@ -33,10 +27,6 @@ public static void bookFlight(Customer cust, Flight flight) {
 	else {
 		
 		Customer.addflight(selectedflight);
-		
-		
-		
-		
 	}
 	//add flight to user flight list
 	//need to add 
@@ -45,21 +35,15 @@ public static void bookFlight(Customer cust, Flight flight) {
 
 
 //method to check if flight if full
-public static boolean isFull(String a) {
+public static boolean isFull(flight a) {
 	
 	//check to see if seating is full
-	if ( seating[60] == true)
-	{
-
+	if ( flight.passengerCount == 10){
 	return true;
 	}
 	else
 		return false;
-	
-		
-	}
-	
-	else
+}
 		
 			
 //cannot book flight more than once, Method to check if flight if booked
@@ -68,28 +52,32 @@ public static boolean isBooked(String a) {
 	
 	for (int i = 0; i <  flights.length;i++ )//is this wrong code, I want the length of the get flights array to iterate thru it.
 		if (flights[i].equals(a));
-			alreadybooked = true
-	
-		return alreadybooked;
-			
+			alreadybooked = true;
+		
+			return false;
+	}
 //delete booked flight from flight array
-public void deleteBooked (string a) {
+public void deleteBooked(String a) {
 	
 	for (int i = 0; i <  flights.length;i++ )
-		if (flights[i].equals(a));
-			flights[i] = null
-	
-//need to add code to 
-					decrease # of passengers
-					
+		if (flights[i].equals(a)) {
+			flights[i] = null;
+		//TODO need to subtract 1 from passenger count for that flight
+		flight.subtract(1)			
 
 }			
 /*schedule conflict, will we be keeping up with a time that flight takes to include this methd? technically it is in the user
 requirements to have both already booked and schedule conflict, but im okay with leaving it out for now just to get a working code*/
 public void scheduleConflict() {
-	
+}
+
+public void deleteFlight(Flight id) {
 	
 }
+public void insertFlight(Flight id) {
+	
+}
+public void updateFlight(Flight id)
 
 }
 }
