e63b267f96ff761dd3621399ba3ece54553ae879
[Bryan] <Bwade@10.0.0.107>
2020-05-06 21:35:51 -0400

    fixes

--- Term Project/src/database/Queries.java
+++ Term Project/src/database/Queries.java
@@ -14,53 +14,7 @@ import common.Flight;
 public class Queries {
 	
 	public static void main(String[] args) {
-		try {
-			getConnection();
-			Customer a = new Customer("John", "Wayne", "jwayne", "programmingHard@gmail.com", "4143567268", "12345",
-					"123 Beat St", "Atlanta", "GA", "30312", "Favorite color?", "678-05-6789", "true", "yellow");
-			INSERT(a);
-			System.out.println("Inserted");
-		} catch(Exception e) {
-			System.out.println("you have a problem with your dbquery insertUser");
-		}
+		
 	}
 
-	public static Connection getConnection() throws Exception {
-
-		try {
-			String driver = "com.mysql.cj.jdbc.Driver";
-			String url = "jdbc:mysql://34.67.46.88:3306/world";
-			String username = "root";
-			String password = "nA97114780!!";
-			Class.forName(driver);
-
-			Connection conn = DriverManager.getConnection(url, username, password);
-			System.out.println("Connected");
-			return conn;
-
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
-		}
-
-	}
-
-	public static final void INSERT(Customer cust) throws Exception{
-		try {
-			
-			Connection con = ConnectionMethod.getConnection();
-			PreparedStatement insert = con.prepareStatement("INSERT INTO `world`.`customer_info` VALUES (6,'" + cust.getUserName() + "', '" + cust.getFirstName() + "', '" + cust.getLastName() + "', '" + cust.getPassword() +
-					"', '" + cust.getAddress() + "', " + cust.getZipcode() + ", '" + cust.getCity() + "', '" + cust.getState() + "', '" + cust.getPhone() + "', '" + cust.getEmail()
-					+ "', '" + cust.getState() + "', " + cust.getsSN() + ", '" + cust.getSecurityQuestion() + "', '" + cust.getSecurityAnswer() +"';");
-			insert.executeUpdate();
-			
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-
-
-	}
-	
-//	//"INSERT INTO `world`.`customer_info` (`first_name`, `last_name`, `username`, `password`, `address`, `zipcode`, `city`, `state`, `phone`, `email`, "
-//	+ "`status`, `ssn`, `security_question`, `security_answer`)"
 }
